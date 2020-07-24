b65687458cc68e09d2b2be1f640818e0ac654740
Gonzalo Matheu <gmatheu@simtlix.com>
2020-07-24 19:31:07 -0300

    actions: Disable documentation (3)

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
                 '-DGDCM_DOCUMENTATION_SKIP_MANPAGES:BOOL=TRUE',
                 '-DGDCM_DOCUMENTATION:BOOL=FALSE',
                 '-DGDCM_BUILD_APPLICATIONS:BOOL=ON',
+                '-DGDCM_BUILD_DOCBOOK_MANPAGES:BOOL=OFF',
                 '-DGDCM_BUILD_SHARED_LIBS:BOOL=OFF'],
     cmake_process_manifest_hook=exclude_static_libraries
 )
